54bd64e48a64af8959b7612a9e1423527819de50
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-05-14 12:14:32 +0600

    dist folder added

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
 import { Pool } from "pg"
+import config from "./config";
 const app: Application = express()
-const port = 5000;
+const port = config.port;
 
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_qsx03YGXHcjT@ep-dawn-boat-aqlzqa4m.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
+  connectionString: config.connection_string
 })
 
 const initDB = async () => {
@@ -193,5 +194,5 @@ app.delete('/api/users/:id', async (req: Request, res: Response) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Database connected successfully port ${port}`)
 })
